4df9d4b69620ecc7ea28bba2ea739e702c4006bd
akankshaudakhe137 <akankshaudakhe137@gmail.com>
2021-04-24 07:58:04 -0700

    Updates

---
+++ precache-manifest.9d70cc886b48dc22ae5431f4ba3834a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fe3324d7c94a0e04957cb8e2113fdd",
+    "url": "/TodoListReactApp/index.html"
+  },
+  {
+    "revision": "aba73f0131d6ddd2a634",
+    "url": "/TodoListReactApp/static/css/main.fdf5c8da.chunk.css"
+  },
+  {
+    "revision": "8a8605308c60690694a1",
+    "url": "/TodoListReactApp/static/js/2.2c4f6304.chunk.js"
+  },
+  {
+    "revision": "aba73f0131d6ddd2a634",
+    "url": "/TodoListReactApp/static/js/main.8fe11428.chunk.js"
+  },
+  {
+    "revision": "82b68d51b90b33d35371",
+    "url": "/TodoListReactApp/static/js/runtime~main.3b005fc4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoListReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
